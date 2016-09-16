b906bc6a81dc37cb733171a4a7dfd7f8cb205cb2
mhourig <mhourig@gmail.com>
2016-09-16 16:34:44 -0500

    draw for two

---
+++ server.js
@@ -0,0 +1,84 @@
+//SERVER SIDE
+// Setup basic express server
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3000;
+
+server.listen(port, function () {
+  console.log('listening at port %d', port);
+});
+
+// Routing
+app.use(express.static(__dirname + '/public'));
+
+var numUsers = 0;
+// WebSocket Portion
+// WebSockets work with the HTTP server
+var io = require('socket.io').listen(server);
+
+// Register a callback function to run when we have an individual connection
+// This is run for each individual user that connects
+io.sockets.on('connection',
+  // We are given a websocket object in our function
+  function (socket) {
+  var addedUser = false;
+    
+//    console.log("We have a new client: " + socket.id);
+    
+    socket.on('new message', function (data) {
+    // we tell the client to execute 'new message'
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
+    });
+  });
+
+    // When this user emits, client side: socket.emit('otherevent',some data);
+    socket.on('mouse',
+      function(data) {
+        // Data comes in as whatever was sent, including objects
+        console.log("x: " + data.x + " y: " + data.y + " " + data.username);
+      
+        // Send it to all other clients
+        socket.broadcast.emit('mouse', data);
+        
+        // This is a way to send to everyone including sender
+        // io.sockets.emit('message', "this goes to everyone");
+
+      }
+    );
+    
+    socket.on('add user', function (username) {
+    if (addedUser) return;
+
+    // we store the username in the socket session for this client
+    socket.username = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+    console.log("welcome " + socket.username);
+  });
+    
+    socket.on('disconnect', function () {
+    if (addedUser) {
+      --numUsers;
+
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+        console.log("goodbye " + socket.username);
+    }
+  });
+  }
+);
